82d37cba0cc3d4026f4190bc1848ec046e1e2ebc
DaleInoc <ddInoc@mcm.edu.ph>
2025-09-30 18:19:20 +0800

    Added core functionality and updated threejs path

--- js/main.js
+++ js/main.js
@@ -1,24 +1,62 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.OrthographicCamera();
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+
+renderer.setSize( 800, 800 );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const geometry = new THREE.PlaneGeometry( 0.3, 0.2);
+const material = new THREE.MeshBasicMaterial();
+const dvd = new THREE.Mesh( geometry, material );
+scene.add( dvd );
 
-camera.position.z = 5;
+let xSpeed = 0.009;//THREE.MathUtils.randFloat(0.0085,0.009);
+let ySpeed = 0.0035;//THREE.MathUtils.randFloat(0.0085,0.009);
+
+camera.position.z = 1;
+dvd.position.set(0,0,0);
+
+updateColor();
+
+function updateColor() {
+    dvd.material.color.setRGB(Math.random(256),Math.random(256),Math.random(256));
+}
+
+function dvdSize(){
+    dvd.scale.x -= 0.1;
+    dvd.scale.y -= 0.1;
+}
 
 function animate() {
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    dvd.position.x += xSpeed;
+    dvd.position.y -= ySpeed;
+    if(dvd.position.x > 0.90){
+        xSpeed -= 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.position.y > 0.90){
+        ySpeed += 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.position.x < -0.90){
+        xSpeed += 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.position.y < -0.90){
+        ySpeed -= 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.scale.x <0 && dvd.scale.y <0){
+        dvd.visible = false;
+    }
 
     renderer.render( scene, camera );
 
