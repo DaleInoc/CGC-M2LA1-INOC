47f5c36ed8b3e89b0ae90bf2fe6de4a5c6c5e9df
DaleInoc <ddInoc@mcm.edu.ph>
2025-10-01 19:10:11 +0800

    Added assets file and moved js and node_modules/three folder into assets

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera();
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( 800, 800 );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.PlaneGeometry( 0.3, 0.2);
+const material = new THREE.MeshBasicMaterial();
+const dvd = new THREE.Mesh( geometry, material );
+scene.add( dvd );
+
+let xSpeed = 0.009;
+let ySpeed = 0.0035;
+
+camera.position.z = 1;
+dvd.position.set(0,0,0);
+
+updateColor();
+
+function updateColor() {
+    dvd.material.color.setRGB(Math.random(256),Math.random(256),Math.random(256));
+}
+
+function dvdSize(){
+    dvd.scale.x -= 0.1;
+    dvd.scale.y -= 0.1;
+}
+
+function animate() {
+
+    dvd.position.x += xSpeed;
+    dvd.position.y -= ySpeed;
+    if(dvd.position.x > 0.90){
+        xSpeed -= 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.position.y > 0.90){
+        ySpeed += 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.position.x < -0.90){
+        xSpeed += 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.position.y < -0.90){
+        ySpeed -= 0.009;
+        dvdSize();
+        updateColor();
+    }
+    if(dvd.scale.x <0 && dvd.scale.y <0){
+        dvd.visible = false;
+    }
+
+    renderer.render( scene, camera );
+
+}
